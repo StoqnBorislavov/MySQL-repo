b29c0270fe4076409d467100679f8994254aaddc
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-05-06 23:20:25 +0300

    Added problem with loop while - with solution

--- FunctionsProceduresTrigersTransactions.sql
+++ FunctionsProceduresTrigersTransactions.sql
@@ -75,6 +75,30 @@ END $$
 DELIMITER ;
 CALL usp_get_employees_by_salary_level('High');	
 
+# 7.	Define Function
+DELIMITER $$
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))
+RETURNS BIT
+DETERMINISTIC
+BEGIN
+	DECLARE result BIT;
+	DECLARE word_length INT;
+    DECLARE current_index INT;
+    
+    SET result := 1;
+    SET word_length := char_length(word);
+    SET current_index := 1;
+    WHILE (current_index <= word_length) DO
+		IF set_of_letters NOT LIKE (concat('%', substr(word, current_index, 1), '%')) THEN
+			SET result := 0;
+        END IF;
+        SET current_index := current_index + 1;
+    END WHILE;
+    RETURN result;
+END $$
+DELIMITER ;
+SELECT ufn_is_word_comprised('oistmiahf', 'halves');
+
 # 8.	Find Full Name
 DELIMITER $$
 CREATE PROCEDURE usp_get_holders_full_name()
